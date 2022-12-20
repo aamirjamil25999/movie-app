026115f600dbbe1e247ea3adecd634c2eeba0da2
Aamir Jamil <aamirjamil25999@gmail.com>
2022-12-20 16:59:48 +0530

    sunscribe to the store

--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './main.css';
+import { legacy_createStore as createStore } from 'redux';
+import movies from './reducers';
+// import { connect } from 'react-redux'
+
+const store = createStore(movies);
+// console.log('beforestate', store.getState());
+
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies: [{ name: 'Superman' }]
+// });
+// console.log('afterstate', store.getState());
+
+// console.log(store.getState());
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App store ={ store } />
   </React.StrictMode>
 );
 
